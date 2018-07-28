0e70ffeb989c81acacff775c81b47da3868f65e9
ProUnebit <ProUnebit@yandex.ru>
2018-07-28 21:43:54 +0300

    add basic react-app build

--- project/src/App.js
+++ project/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <div className="app-title">MusicMaster</div>
+                <div>
+                    <input placeholder="Search an artists..."/>
+                    <button>Button</button>
+                </div>
+                <div className="profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="gallery">Gallery</div>
+            </div>
+        )
+    }
 }
 
 export default App;
